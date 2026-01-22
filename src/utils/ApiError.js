d212567f3a702f0d3ad7defdef5fdec1e47f85dd
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-01-22 22:37:21 +0600

    update api error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,9 +1,9 @@
 class ApiError extends Error {
   constructor(
     statusCode,
-    message = "Something Went Wrong",
+    message = "Something Went Wrong!",
     errors = [],
-    stack = ""
+    stack = "",
   ) {
     super(message);
     this.statusCode = statusCode;
